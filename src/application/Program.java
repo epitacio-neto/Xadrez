c65794d1b020fafa12b74d88976af5dedc4ed03b
neto0w <epitacio.neto.92@gmail.com>
2023-11-26 16:47:32 -0300

    primeira classe position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,20 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!");
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
+		
+		sc.close();
 	}
 
 }
